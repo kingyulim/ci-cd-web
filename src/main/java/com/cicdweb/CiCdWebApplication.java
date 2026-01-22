23c6470c587b423544a4b2ce5da837bf3a4ca453
Kingyulim <youremail@example.com>
2026-01-22 16:05:37 +0900

    Initial commit

---
+++ src/main/java/com/cicdweb/CiCdWebApplication.java
@@ -0,0 +1,13 @@
+package com.cicdweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiCdWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CiCdWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cicdweb/CiCdWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cicdweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiCdWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
